32eeb7bc4e46f43e73b343f96764531b12f4e143
valenthyne-ut <valenthyne.ut@gmail.com>
2023-12-23 13:56:26 +0000

    Create account component

---
+++ src/components/CreateAccountComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const username = ref<string | undefined>(undefined);
+const password = ref<string | undefined>(undefined);
+</script>
+
+<template>
+	<div>
+		<button type="button">Create new account</button>
+		<div class="account-details" v-if="username && password">
+			<br>Account details:
+			<br><span><strong>Username:</strong> {{ username }}</span>
+			<br><span><strong>Password:</strong> {{ password }}</span>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+div.account-details > span {
+	margin-left: 1em;
+}
+</style>
--- src/views/CreateAccountView.vue
+++ src/views/CreateAccountView.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import CreateAccountComponent from "@/components/CreateAccountComponent.vue";
 </script>
 
 <template>
 	<main>
-
+		<CreateAccountComponent/>
 	</main>
 </template>
