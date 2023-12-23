d1a0d900ebf99933a97c564f66a5645d90eb026c
valenthyne-ut <valenthyne.ut@gmail.com>
2023-12-23 15:28:24 +0000

    Add submit button API interaction

--- src/components/LoginFormComponent.vue
+++ src/components/LoginFormComponent.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import { AuthAPI } from "@/classes/api/Auth.api";
+
+const username = ref<string>("");
+const password = ref<string>("");
+const errorText = ref<string>("");
+
+const authApi = new AuthAPI();
+
+const attemptLogin = async () => {
+	try {
+		await authApi.login(username.value, password.value);
+	} catch(error) {
+		errorText.value = error as string;
+	}
+};
 </script>
 
 <template>
 	<form>
-		<p class="error-message"></p>
+		<p class="error-message">{{ errorText }}</p>
 		<label for="username">Username</label><br>
 		<input type="text" name="username"><br><br>
 		<label for="password">Password</label><br>
 		<input type="password" name="password"><br><br>
-		<button type="button">Submit</button>
+		<button type="button" @click="attemptLogin">Submit</button>
 	</form>
 </template>
 
