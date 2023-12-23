352fe6ab9bb0b29b8db5e3307ad6a2eea9cff8d3
valenthyne-ut <valenthyne.ut@gmail.com>
2023-12-23 13:35:10 +0000

    Login form component

---
+++ src/components/LoginFormComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<form>
+		<p class="error-message"></p>
+		<label for="username">Username</label><br>
+		<input type="text" name="username"><br><br>
+		<label for="password">Password</label><br>
+		<input type="password" name="password"><br><br>
+		<button type="button">Submit</button>
+	</form>
+</template>
+
+<style scoped>
+p.error-message {
+	color: red;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import LoginFormComponent from "@/components/LoginFormComponent.vue";
 </script>
 
 <template>
 	<main>
-		
+		<LoginFormComponent/>
 	</main>
 </template>
