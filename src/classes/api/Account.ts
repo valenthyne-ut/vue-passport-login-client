5815ebfc11baf90450daeb0e02445accf7ba37fa
valenthyne-ut <valenthyne.ut@gmail.com>
2023-12-29 18:00:28 +0000

    Implement receiving and displaying account details

--- src/classes/api/Account.ts
+++ src/classes/api/Account.ts
@@ -1,11 +1,33 @@
 import { BaseAPI } from ".";
 
+export interface CreateAccountResponse {
+	username: string;
+	password: string;
+	roles: Array<string>;
+}
+
 export class AccountAPI extends BaseAPI {
 	constructor() {
 		super(BaseAPI.BASEAPI_DEFAULT_PATH_STRING + "/account");
 	}
 
-	async createAccount(role: string): Promise<User> {
-		throw new Error("Not implemented.");
+	async createAccount(role: string): Promise<CreateAccountResponse | string> {
+		const requestData = JSON.stringify({ 
+			"role": role 
+		});
+		const requestOptions: RequestInit = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: requestData
+		};
+		const response = await fetch(this.apiPath, requestOptions);
+		if(response.ok) { 
+			return await response.json();
+		} else {
+			return await response.text();
+		}
+
 	}
 }
--- src/components/CreateAccountComponent.vue
+++ src/components/CreateAccountComponent.vue
@@ -1,26 +1,59 @@
 <script setup lang="ts">
+import { AccountAPI } from "@/classes/api/Account";
 import { ref } from "vue";
 
-const username = ref<string | undefined>(undefined);
-const password = ref<string | undefined>(undefined);
+const username = ref<string>("");
+const password = ref<string>("");
+const roles = ref<string[]>([]);
+const selectedRole = ref<string>("unselected");
+const errorText = ref<string>("");
+
+const accountAPI = new AccountAPI();
+const createNewAccount = async () => {
+	errorText.value = "";
+	try {
+		const result = await accountAPI.createAccount(selectedRole.value);
+		if(typeof result === "string") {
+			errorText.value = result;
+		} else {
+			username.value = result.username;
+			password.value = result.password;
+			roles.value = result.roles;
+		}
+	} catch(error) {
+		console.log(error);
+	}
+};
 </script>
 
 <template>
 	<div>
-		<label for="role_select_admin">Admin</label>
-		<input type="radio" name="role_select" id="role_select_admin" value="admin">
+		<p class="error-message">{{ errorText }}</p>
 		<label for="role_select_user">User</label>
-		<input type="radio" name="role_select" id="role_select_user" value="user"><br>
-		<button type="button">Create new account</button>
+		<input type="radio" name="role_select" id="role_select_user" v-model="selectedRole" value="user">
+		<label for="role_select_admin">Admin</label>
+		<input type="radio" name="role_select" id="role_select_admin" v-model="selectedRole" value="admin"><br>
+		<button type="button" @click="createNewAccount">Create new account</button>
+
 		<div class="account-details" v-if="username && password">
-			<br>Account details:
-			<br><span><strong>Username:</strong> {{ username }}</span>
-			<br><span><strong>Password:</strong> {{ password }}</span>
+			<br>Account details:<br>
+			<span>
+				<strong>Username:</strong> {{ username }}
+			</span><br>
+			<span>
+				<strong>Password:</strong> {{ password }}
+			</span><br>
+			<span>
+				<strong>Roles:</strong> <ul><li v-for="role, index in roles" :key="index">{{ role }}</li></ul>
+			</span>
 		</div>
 	</div>
 </template>
 
 <style scoped>
+p.error-message {
+	color: red;
+}
 div.account-details > span {
 	margin-left: 1em;
 }
