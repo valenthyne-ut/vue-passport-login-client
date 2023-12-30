027230693c1aa81ded4efc54641db61d5cd29e55
valenthyne-ut <valenthyne.ut@gmail.com>
2023-12-30 17:59:05 +0000

    Export authState store

--- src/stores/authState.store.ts
+++ src/stores/authState.store.ts
@@ -2,7 +2,7 @@ import { User } from "@/classes/User";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-const useAuthStateStore = defineStore("authState", () => {
+export const useAuthStateStore = defineStore("authState", () => {
 	const authenticated = ref(false);
 	const user = ref<User | undefined>(undefined);
 
