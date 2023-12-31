6ff9a91b961bcfd988860fbe65110d35fe78718a
valenthyne-ut <valenthyne.ut@gmail.com>
2023-12-31 13:03:22 +0000

    Moving JWT to and from localStorage on exit/enter

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,31 @@
 <script setup lang="ts">
+import { onBeforeMount } from "vue";
+import { useAuthStateStore } from "./stores/authState.store";
+import { AuthAPI } from "./classes/api/Auth";
+import router from "./router";
+
+const authAPI = new AuthAPI();
+const authStore = useAuthStateStore();
+
+onBeforeMount(async () => {
+	window.addEventListener("beforeunload", () => {
+		if(authStore.jwtToken) { localStorage.setItem("jwtToken", authStore.jwtToken); }
+	});
+
+	const jwtToken = localStorage.getItem("jwtToken");
+	if(jwtToken && !authStore.authenticated) {
+		try {
+			const result = await authAPI.getAuthDetails(jwtToken);
+			authStore.authenticated = true;
+			authStore.jwtToken = jwtToken;
+			authStore.user = result.user;
+			localStorage.removeItem("jwtToken");
+			router.push({ path: "/" });
+		} catch(error) {
+			console.log(error);
+		}
+	}
+});
 </script>
 
 <template>
