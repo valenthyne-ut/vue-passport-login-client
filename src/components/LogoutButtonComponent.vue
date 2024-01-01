a2c7cc6eaa299e9887913596b1be839bcdf1f3e2
valenthyne-ut <valenthyne.ut@gmail.com>
2024-01-01 15:03:23 +0000

    Implement logout functionality in button

--- src/components/LogoutButtonComponent.vue
+++ src/components/LogoutButtonComponent.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
-const logout = () => {
+import { AuthAPI } from "@/classes/api/Auth";
+import router from "@/router";
+import { useAuthStateStore } from "@/stores/authState.store";
 
+const authApi = new AuthAPI();
+const authStore = useAuthStateStore();
+
+const logout = async () => {
+	try {
+		await authApi.logout(authStore.jwtToken);
+
+		authStore.authenticated = false;
+		authStore.jwtToken = "";
+		authStore.user = undefined;
+
+		router.push({ path: "/login" });
+	} catch(error) {
+		console.log(error as string);
+	}
 };
 </script>;
 
