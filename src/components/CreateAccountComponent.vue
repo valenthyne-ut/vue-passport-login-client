f5a7c96c242e9104fa2bc971753c880285c8f88a
valenthyne-ut <valenthyne.ut@gmail.com>
2024-01-04 15:21:48 +0000

    Rework create account screen

--- src/components/CreateAccountComponent.vue
+++ src/components/CreateAccountComponent.vue
@@ -17,31 +17,45 @@
 			password.value = result.password;
 			roles.value = result.roles;
 		} catch(error) {
-			errorText.value = error as string;
+			if(error instanceof Error) {
+				errorText.value = error.message;
+			} else if(typeof error === "string") {
+				errorText.value = error;
+			}
 		}
 	};
 </script>
 
 <template>
-	<div>
-		<p class="error-message">{{ errorText }}</p>
-		<label for="role_select_user">User</label>
-		<input type="radio" name="role_select" id="role_select_user" v-model="selectedRole" value="user">
-		<label for="role_select_admin">Admin</label>
-		<input type="radio" name="role_select" id="role_select_admin" v-model="selectedRole" value="admin"><br>
-		<button type="button" @click="createNewAccount">Create new account</button>
-
-		<div class="account-details" v-if="username && password">
-			<br>Account details:<br>
-			<span>
-				<strong>Username:</strong> {{ username }}
-			</span><br>
-			<span>
-				<strong>Password:</strong> {{ password }}
-			</span><br>
-			<span>
-				<strong>Roles:</strong> <ul><li v-for="role, index in roles" :key="index">{{ role }}</li></ul>
-			</span>
+	<div class="h-screen flex justify-center items-center">
+		<div class="w-96 bg-white p-7 rounded-sm shadow-sm">
+			<div class="bg-red-200 border rounded border-red-400 text-center p-4 mb-4" v-if="errorText">
+				<span class="text-red-950">{{ errorText }}</span>
+			</div>
+			<RouterLink class="text-gray-400 text-sm" to="/login">Back to login</RouterLink>
+			<div class="w-full flex justify-center space-x-4 mt-2">
+				<div>
+					<label class="select-none" for="role_select_user">User </label>
+					<input type="radio" name="role_select" id="role_select_user" value="user" v-model="selectedRole">
+				</div>
+				<div>
+					<label class="select-none" for="role_select_admin">Admin </label>
+					<input type="radio" name="role_select" id="role_select_admin" value="admin" v-model="selectedRole">
+				</div>
+			</div>
+			<button class="w-full border rounded-md bg-blue-600 text-white text-xl py-2 mt-4 mb-2" type="button" @click="createNewAccount">Create account</button>
+			<div class="flex flex-col" v-if="username && password && roles">
+				<hr class="my-4">
+				<span>
+					<strong>Username: </strong> {{ username }}
+				</span>
+				<span>
+					<strong>Password: </strong> {{ password }}
+				</span>
+				<span>
+					<strong>Role(s): </strong> {{ roles.join(", ") }}
+				</span>
+			</div>
 		</div>
 	</div>
 </template>
